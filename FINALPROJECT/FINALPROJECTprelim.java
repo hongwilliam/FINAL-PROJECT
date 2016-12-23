ee936bbdb45b7c37e0eb78b715eee5f302e56703
William Hong <willhong20@gmail.com>
2016-12-23 03:25:37 -0500

    This is just a placeholder file

---
+++ FINALPROJECT/FINALPROJECTprelim.java
@@ -0,0 +1,9 @@
+public class FINALPROJECTprelim{
+	
+	public static void main(String[] args){
+		System.out.println("Preliminary data");
+		System.out.println("My own personal prediction: New England Patriots 21, Seattle Seahawks 17... let's see if my project will produce a similar result!");
+
+	}
+
+}
